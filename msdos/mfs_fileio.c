9fbcbce66edb52b990220b05742b74feac6a478f
nepx <23300488+nepx@users.noreply.github.com>
2019-12-22 21:32:52 -0800

    Fix improperly split lines

--- msdos/mfs_fileio.c
+++ msdos/mfs_fileio.c
@@ -213,8 +213,7 @@ mfs_delete(DWORD p1, DWORD p2, DWORD p3, DWORD p4)
            (strcmp(filename, xdos.fileio[slot].filename) == 0) ) {
           /* Get the handle */
           if ((handle = gethandle()) < 0) {
-              ERRSTR((LF_ERROR,"mfs_delete cannot get handle for file %s %d
- %d\n", filename, errno, slot));
+              ERRSTR((LF_ERROR,"mfs_delete cannot get handle for file %s %d %d\n", filename, errno, slot));
               return MAKELONG(NO_HANDLES & 0xffff,0xffff);
           }
           xdos.fileptr[handle] = &xdos.fileio[slot];
@@ -232,8 +231,7 @@ mfs_delete(DWORD p1, DWORD p2, DWORD p3, DWORD p4)
      	       return unlink(filename);
               }
           }
-          else ERRSTR((LF_ERROR,"mfs_delete : invalid handle %d for file
-%s\n",handle, filename));
+          else ERRSTR((LF_ERROR,"mfs_delete : invalid handle %d for file %s\n",handle, filename));
          }
      }
      /* At this point, either the file was fclosed by us, or it was
